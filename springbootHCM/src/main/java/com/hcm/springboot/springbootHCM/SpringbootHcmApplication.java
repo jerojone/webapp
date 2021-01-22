afa118e77a084c9e042c49b46e1f043ae4a69b0e
IPL <IPL@Payroll_1>
2021-01-22 09:11:39 +0530

    test commit

---
+++ springbootHCM/src/main/java/com/hcm/springboot/springbootHCM/SpringbootHcmApplication.java
@@ -0,0 +1,13 @@
+package com.hcm.springboot.springbootHCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHcmApplication.class, args);
+	}
+
+}
---
+++ springbootHCM/src/test/java/com/hcm/springboot/springbootHCM/SpringbootHcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcm.springboot.springbootHCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
