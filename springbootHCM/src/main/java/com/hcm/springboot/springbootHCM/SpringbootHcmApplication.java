f5006344ba1a23cd1bdc7b55c448a8366451e4f0
IPL <IPL@Payroll_1>
2021-01-22 11:25:01 +0530

    level-2

--- springbootHCM/src/main/java/com/hcm/springboot/springbootHCM/SpringbootHcmApplication.java
+++ springbootHCM/src/main/java/com/hcm/springboot/springbootHCM/SpringbootHcmApplication.java
@@ -2,12 +2,17 @@ package com.hcm.springboot.springbootHCM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableJpaRepositories(basePackages = "com.spring.webapp.db")
 @SpringBootApplication
+@ComponentScan("com.*")
 public class SpringbootHcmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHcmApplication.class, args);
+		
 	}
 
 }
